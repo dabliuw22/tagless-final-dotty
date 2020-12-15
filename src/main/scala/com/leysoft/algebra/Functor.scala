976bb55f40e6e4e5936c6b9338ae173a973a71ad
Will Leyton <wleylop22@gmail.com>
2020-12-15 08:05:30 -0500

    Add algebra Functor

---
+++ src/main/scala/com/leysoft/algebra/Functor.scala
@@ -0,0 +1,9 @@
+package com.leysoft.algebra
+
+trait Functor[F[_]] {
+  def fmap[A, B](fa: F[A])(f: A => B): F[B]
+}
+
+object Functor {
+  inline def apply[F[_]](using F: Functor[F]): Functor[F] = F
+}
