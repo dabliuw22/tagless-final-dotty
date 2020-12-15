3405d8d8bfc2361a2383c4c3e435d2dfba654e59
Will Leyton <wleylop22@gmail.com>
2020-12-15 08:05:43 -0500

    Add algebra Apply

---
+++ src/main/scala/com/leysoft/algebra/Apply.scala
@@ -0,0 +1,11 @@
+package com.leysoft.algebra
+
+trait Apply[F[_]] extends Functor[F] {
+  def ap[A, B](ff: F[A => B])(fa: F[A]): F[B]
+  def productR[A, B](fa: F[A])(fb: F[B]): F[B] =
+    ap(fmap(fa)(_ => (b: B) => b))(fb)
+}
+
+object Apply {
+  inline def apply[F[_]](using F: Apply[F]): Apply[F] = F
+}
