4c05ac8ba02d516dfbe797ca1d059ae95fcf89ad
Will Leyton <wleylop22@gmail.com>
2020-12-15 08:05:52 -0500

    Add algebra Applicative

---
+++ src/main/scala/com/leysoft/algebra/Applicative.scala
@@ -0,0 +1,10 @@
+package com.leysoft.algebra
+
+trait Applicative[F[_]] extends Apply[F] {
+  def pure[A](x: A): F[A]
+  def unit: F[Unit] = pure(())
+}
+
+object Applicative {
+  inline def apply[F[_]](using F: Applicative[F]): Applicative[F] = F
+}
