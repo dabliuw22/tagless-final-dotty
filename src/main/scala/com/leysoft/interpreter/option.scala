b0e0df8d659e7b354452a6aa654812b72e168e9b
Will Leyton <wleylop22@gmail.com>
2020-12-16 09:00:43 -0500

    Update

--- src/main/scala/com/leysoft/interpreter/option.scala
+++ src/main/scala/com/leysoft/interpreter/option.scala
@@ -5,8 +5,8 @@ import com.leysoft.algebra.{Applicative, Apply, Functor}
 object option {
 
   given instance as Applicative[Option]
-    with Apply[Option]
-    with Functor[Option]:
+      with Apply[Option]
+      with Functor[Option]:
     override def pure[A](x: A): Option[A] = Some(x)
     
     override def ap[A, B](ff: Option[A => B])(fa: Option[A]): Option[B] =
