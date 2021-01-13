def56a190082c94d01dd63e459277ac40b4dfada
Will Leyton <wleylop22@gmail.com>
2021-01-13 08:07:32 -0500

    Update Applicative

--- src/main/scala/com/leysoft/algebra/Applicative.scala
+++ src/main/scala/com/leysoft/algebra/Applicative.scala
@@ -1,10 +1,8 @@
 package com.leysoft.algebra
 
-trait Applicative[F[_]] extends Apply[F] {
+trait Applicative[F[_]] extends Apply[F]:
   def pure[A](x: A): F[A]
   def unit: F[Unit] = pure(())
-}
 
-object Applicative {
+object Applicative:
   inline def apply[F[_]](using F: Applicative[F]): Applicative[F] = F
-}
