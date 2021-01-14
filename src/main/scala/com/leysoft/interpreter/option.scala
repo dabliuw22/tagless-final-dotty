9b435b2a64444ef342bc737d0425b4cec0391485
Will Leyton <wleylop22@gmail.com>
2021-01-14 08:39:02 -0500

    Add option interpreter

--- src/main/scala/com/leysoft/interpreter/option.scala
+++ src/main/scala/com/leysoft/interpreter/option.scala
@@ -2,18 +2,14 @@ package com.leysoft.interpreter
 
 import com.leysoft.algebra.{Applicative, Apply, Functor}
 
-object option {
-
-  given instance as Applicative[Option]
-      with Apply[Option]
-      with Functor[Option]:
+object option:
+  given instance: Applicative[Option] 
+    with Apply[Option] 
+    with Functor[Option] with
     override def pure[A](x: A): Option[A] = Some(x)
-    
     override def ap[A, B](ff: Option[A => B])(fa: Option[A]): Option[B] =
       ff match
         case Some(f) => fmap(fa)(f)
         case _       => None
-
     override def fmap[A, B](fa: Option[A])(f: A => B): Option[B] =
       fa.map(f)
-}
